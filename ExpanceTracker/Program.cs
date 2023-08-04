f11833ee8b7027b65d7b35b1d76ac69626984a9a
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-08-04 13:42:21 +0200

    DI

--- ExpanceTracker/Program.cs
+++ ExpanceTracker/Program.cs
@@ -1,7 +1,14 @@
+using ExpanceTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DI
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
 
 var app = builder.Build();
 
